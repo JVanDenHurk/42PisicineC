13b68bf9df9f5ec173929ffce67c28125372bd6b
Justin VanDenHurk <justin.t.hurk@gmail.com>
2022-05-30 09:40:02 +0930

    adding c03

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -6,38 +6,38 @@
 /*   By: justin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 10:55:09 by justin            #+#    #+#             */
-/*   Updated: 2022/05/29 16:30:45 by justin           ###   ########.fr       */
+/*   Updated: 2022/05/30 09:30:51 by jvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+//#include <unistd.h>
 
-
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-	if(nb == -2147483648)
+	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
 		return ;
 	}
-	if(nb < 0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if(nb > 9)
+	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 	}
 	ft_putchar(nb % 10 + '0');
 }
-
+/*
 int main(void)
 {
 	ft_putnbr(-2147483648);
 }
+*/
