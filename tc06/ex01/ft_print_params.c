499e2e0eea486a6b8ef53028e76c70b9d0de4b0c
Justin VanDenHurk <justin.t.hurk@gmail.com>
2022-06-03 12:21:55 +0930

    42

--- tc06/ex01/ft_print_params.c
+++ tc06/ex01/ft_print_params.c
@@ -6,21 +6,20 @@
 /*   By: jvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:08:14 by jvan-den          #+#    #+#             */
-/*   Updated: 2022/06/02 17:14:20 by jvan-den         ###   ########.fr       */
+/*   Updated: 2022/06/03 12:20:12 by jvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	int	i;
 	int	c;
 
 	i = 0;
 	c = 0;
-
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		c = str[i];
 		write(1, &c, 1);
@@ -28,7 +27,16 @@ void ft_putstr(char *str)
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	ft_putstr(argv[1][2][3]);
+	int	b;
+
+	b = 1;
+	while (b < argc)
+	{
+		ft_putstr(argv[b]);
+		b++;
+		write(1, "\n", 1);
+	}
+	return (0);
 }
